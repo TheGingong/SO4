cb07abaf2dda82c3be14fed55dd07e6378c09678
williambauer <william.bauer02@gmail.com>
2020-11-17 14:26:17 +0100

    Differentialregning

--- plot.py
+++ plot.py
@@ -4,23 +4,35 @@ from Integralregning import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-class plot():
-    def __init__(self, xZero):
-        self.xZero = xZero
+#print(object.a, object.b, object.c, object.xZero, object.formel)
 
-    #laver graf fra x -3 til 3, og laver 1000 mellem tal imellem de 2 punkter.
-    x = np.linspace (-3,3, 1000)
-    #her sætter vi vores x værdi
-    def f(self,xZero):
-        self.xZero = xZero
-        return self.xZero**2
+class plot(differential):
+    def __init__(self):
+        print(object.b)
+        plt.plot([5,object.b])
+        plt.show()
 
-    #plotter funktion ind
-    plt.plot(x,f(self, xZero))
+plot()
 
-    #viser vores funktion
-    plt.show()
 
-    #https://www.youtube.com/watch?v=u5VCZBUNOcA
 
-homo = plot(xZero)
+
+"""
+# laver graf fra x -3 til 3, og laver 1000 mellem tal imellem de 2 punkter.
+x = np.linspace(-3, 3, 1000)
+
+
+# her sætter vi vores x værdi
+def f(self, xZero):
+    self.xZero = xZero
+    return self.xZero ** 2
+
+
+# plotter funktion ind
+plt.plot(x, f(self, xZero))
+
+# viser vores funktion
+plt.show()
+
+# https://www.youtube.com/watch?v=u5VCZBUNOcA
+"""
