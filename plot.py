0a13f0e93c15811bb882a97484541e7e8193b396
williambauer <william.bauer02@gmail.com>
2020-11-17 13:14:49 +0100

    Differentialregning

--- plot.py
+++ plot.py
@@ -5,23 +5,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class plot():
-    def __init__(self):
-        pass
+    def __init__(self, xZero):
+        self.xZero = xZero
 
-#laver graf fra x -3 til 3, og laver 1000 mellem tal imellem de 2 punkter.
-x = np.linspace (-3,3, 1000)
-#her sætter vi vores x værdi
-def f(self,xZero):
-    self.xZero = xZero
-    return self.xZero**2
+    #laver graf fra x -3 til 3, og laver 1000 mellem tal imellem de 2 punkter.
+    x = np.linspace (-3,3, 1000)
+    #her sætter vi vores x værdi
+    def f(self,xZero):
+        self.xZero = xZero
+        return self.xZero**2
 
-#plotter funktion ind
-plt.plot(x,f(self,xZero))
+    #plotter funktion ind
+    plt.plot(x,f(self, xZero))
 
-#viser vores funktion
-plt.show()
+    #viser vores funktion
+    plt.show()
 
-#https://www.youtube.com/watch?v=u5VCZBUNOcA
+    #https://www.youtube.com/watch?v=u5VCZBUNOcA
 
-
-ddddddddddd
+homo = plot(xZero)
