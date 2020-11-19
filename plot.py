cc7aa835c61edddbb48f774e8c9db77a409a3762
jacob <jacob@siebert.dk>
2020-11-19 11:11:19 +0100

    plox

--- plot.py
+++ plot.py
@@ -3,6 +3,7 @@ from differential import *
 from Integralregning import *
 import matplotlib.pyplot as plt
 import numpy as np
+#from sympy import *
 
 #print(object.a, object.b, object.c, object.xZero, object.formel)
 
@@ -10,18 +11,22 @@ class plot(differential):
     def __init__(self):
         print(str(object.a) + ("x^2") + " + " + str(object.b) + "x" + " + " + str(object.c))
 
-    def funktionsforskrift(x):
-        return ((object.a * x**2) + (object.b*x) + object.c)
+    #def funktionsforskrift(x):
+    #    x = Symbol('x')
+    #    return ((object.a * x**2) + (object.b*x) + object.c)
+
+    def f(x):
+        return x ** 2
 
     x = np.linspace(-3, 3, 1000)
-    plt.plot(x, funktionsforskrift(x))
+    plt.plot(x, f(x))
     plt.show()
 
 plot()
 
 
 """
-# laver graf fra x -3 til 3, og laver 1000 mellem tal imellem de 2 punkter.
+# laver graf fra x -3 til 3, og laver 1000 mellem x værdiger imellem de 2 punkter.
 x = np.linspace(-3, 3, 1000)
 
 
