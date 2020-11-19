e1e48f3070d56dabdf615770259c574e3aa5f8d1
williambauer <william.bauer02@gmail.com>
2020-11-19 12:35:25 +0100

    Push af integralregning samt differential regning opdatering

--- plot.py
+++ plot.py
@@ -11,19 +11,10 @@ class plot(differential, integralregning):
             # Her laver vi selve funktionsforskriften fra a, b og c.
             return ((a * x ** 2) + (b * x) + c)
 
-
-
-
         xer = np.linspace(-3, 3, 1000)
         yer = f(objectIntegralregning.a, xer, objectIntegralregning.b, objectIntegralregning.c)
         plt.plot(xer, yer)
         plt.fill_between(xer, yer, 0, color="red", alpha=0.5)
-
-        ax = plt.subplots()
-        #ax.text(0.5 * (objectIntegralregning.a + objectIntegralregning.b), 30, r"$\int_a^b f(x)\mathrm{d}x$",
-         #       horizontalalignment='center', fontsize=20)
-        ax.plot(xer, yer, 'r', linewidth=2)
-        ax.set_ylim(bottom=0)
 
         plt.show()
 plot()
