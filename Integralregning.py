51910a32b96805f64e7cb98ad9587313ff40f6e5
jacob <jacob@siebert.dk>
2020-11-20 10:06:55 +0100

    Vlines

--- Integralregning.py
+++ Integralregning.py
@@ -3,35 +3,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 
-#Vi har snakket med Casper, Frederik og David omkring hvordan de gjorde deres matematiske regneregler, og
-#brugte den viden til at komme videre da vi sad fast.
 
-#Vi har valgt at følge lidt den samme stil som vi skrev i differentialregning da vi følte at det gav mere mening i forhold til
-#formlerne som vi fik vidst i matematik.
+# Vi har snakket med Casper, Frederik og David omkring hvordan de gjorde deres matematiske regneregler, og
+# brugte den viden til at komme videre da vi sad fast.
+
+# Vi har valgt at følge lidt den samme stil som vi skrev i differentialregning da vi følte at det gav mere mening i forhold til
+# formlerne som vi fik vidst i matematik.
 
 class integralregning():
-    #I init funktionen modtager vi en a-, b-, c værdi samt en værdi for hvor mange søjler der skal være
-    #for at regne det mest præcise areal ud.
+    # I init funktionen modtager vi en a-, b-, c værdi samt en værdi for hvor mange søjler der skal være
+    # for at regne det mest præcise areal ud.
     def __init__(self, a, b, c, columns):
         self.a = a
         self.b = b
         self.c = c
         self.columns = columns
 
-    #I denne funktion bliver vores funktionsforskrift skrevet udfra hvad a, b, c,
+    # I denne funktion bliver vores funktionsforskrift skrevet udfra hvad a, b, c,
     def func(self, x):
         # Her laver vi selve funktionsforskriften vha. a, b og c som også ses i "differential.py".
         return ((self.a * x ** 2) + (self.b * x) + self.c)
 
-    #Denne funktion benytter vi det princip omkring integralregning og hvordan vi definere hvordan
-    #arealet skal udregnes.
+    # Denne funktion benytter vi det princip omkring integralregning og hvordan vi definere hvordan
+    # arealet skal udregnes.
     def integral(self, xZero, xOne):
         self.xZero = xZero
         self.xTwo = xOne
         self.xOne = xOne
 
         self.deltaX = (self.xOne - self.xZero) / self.columns
- 
+
         self.sum = 0
         n = 0
         while True:
@@ -46,6 +47,7 @@ class integralregning():
             if n == self.columns:
                 print(self.sum)
                 break
+
 
 objectIntegralregning = integralregning(5, 6, 10, 10000)
 objectIntegralregning.integral(3, 10)
--- plot.py
+++ plot.py
@@ -12,13 +12,22 @@ class plot(differential, integralregning):
             return ((a * x ** 2) + (b * x) + c)
 
         lower = objectIntegralregning.xZero
-        upper = objectIntegralregning.xOne
+        upper = objectIntegralregning.xTwo
         print(lower,upper)
+        #Plot af integralregning
         xer = np.linspace(-15, 15, 1000)
         yer = f(objectIntegralregning.a, xer, objectIntegralregning.b, objectIntegralregning.c)
         #ax = plt.subplots()
         plt.plot(xer, yer)
         plt.fill_between(xer, yer, 0, color="red", alpha=0.5)
         plt.vlines(x=[lower,upper],ymin=0,ymax=[f(objectIntegralregning.a, lower, objectIntegralregning.b, objectIntegralregning.c),f(objectIntegralregning.a, upper, objectIntegralregning.b, objectIntegralregning.c)], color= "green")
+        plt.show()
+
+
+        #Plot af differentialregningen
+        xerDiff = np.linspace(objectDiff.xZero, objectDiff.xTwo, 1000)
+        yerDiff = self.tangentPåLinjen(xer)
+        plt.plot(xerDiff, yerDiff)
+        plt.plot(xerDiff, f(objectDiff.a, xer, objectDiff.b, objectDiff.c))
         plt.show()
 plot()
