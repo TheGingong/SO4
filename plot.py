35cbe5b065e518fa86837288431b40727598fcef
jacob <jacob@siebert.dk>
2020-11-20 10:41:21 +0100

    Vlines and fill

--- plot.py
+++ plot.py
@@ -11,21 +11,24 @@ class plot(differential, integralregning):
             # Her laver vi selve funktionsforskriften fra a, b og c.
             return ((a * x ** 2) + (b * x) + c)
 
-        lower = objectIntegralregning.xZero
-        upper = objectIntegralregning.xTwo
-        print(lower,upper)
+        lower = objectIntegralregning.xZeroTwo
+        upper = objectIntegralregning.xOneTwo
+
         #Plot af integralregning
         xer = np.linspace(-15, 15, 1000)
         yer = f(objectIntegralregning.a, xer, objectIntegralregning.b, objectIntegralregning.c)
+        filx1 = np.linspace(lower, upper, 100)
+        fily = f(objectIntegralregning.a, filx1, objectIntegralregning.b, objectIntegralregning.c)
         #ax = plt.subplots()
         plt.plot(xer, yer)
-        plt.fill_between(xer, yer, 0, color="red", alpha=0.5)
+        #plt.fill_between(xer, yer, 0, color="red", alpha=0.5)
         plt.vlines(x=[lower,upper],ymin=0,ymax=[f(objectIntegralregning.a, lower, objectIntegralregning.b, objectIntegralregning.c),f(objectIntegralregning.a, upper, objectIntegralregning.b, objectIntegralregning.c)], color= "green")
+        plt.fill_between(filx1, fily, 0, color="red", alpha=0.5)
         plt.show()
 
 
         #Plot af differentialregningen
-        xerDiff = np.linspace(objectDiff.xZero, objectDiff.xTwo, 1000)
+        xerDiff = np.linspace(objectDiff.xZero, objectDiff.xOneTwo, 1000)
         yerDiff = self.tangentPåLinjen(xer)
         plt.plot(xerDiff, yerDiff)
         plt.plot(xerDiff, f(objectDiff.a, xer, objectDiff.b, objectDiff.c))
