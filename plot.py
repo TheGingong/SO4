4c9c89fe9b754a1b34d27f02dc20e0a2fbeb46d4
jacob <jacob@siebert.dk>
2020-11-20 09:56:36 +0100

    Merge branch 'master' of /Users/jacob/PycharmProjects/SO4 with conflicts.

--- plot.py
+++ plot.py
@@ -11,24 +11,14 @@ class plot(differential, integralregning):
             # Her laver vi selve funktionsforskriften fra a, b og c.
             return ((a * x ** 2) + (b * x) + c)
 
-        a = objectIntegralregning.xZero
-        b = objectIntegralregning.xOne
-
-        #Plot af integralregning
+        lower = objectIntegralregning.xZero
+        upper = objectIntegralregning.xOne
+        print(lower,upper)
         xer = np.linspace(-15, 15, 1000)
         yer = f(objectIntegralregning.a, xer, objectIntegralregning.b, objectIntegralregning.c)
         #ax = plt.subplots()
         plt.plot(xer, yer)
         plt.fill_between(xer, yer, 0, color="red", alpha=0.5)
-        plt.vlines(x=[a,b],ymin=0,ymax=[100,100])
-        plt.show()
-
-        print(objectDiff.xOne)
-        print(objectDiff.xZero)
-        #Plot af differentialregningen
-        xerDiff = np.linspace(objectDiff.xZero, objectDiff.xTwo, 1000)
-        yerDiff = self.tangentPåLinjen(xer)
-        plt.plot(xerDiff, yerDiff)
-        plt.plot(xerDiff, f(objectDiff.a, xer, objectDiff.b, objectDiff.c))
+        plt.vlines(x=[lower,upper],ymin=0,ymax=[f(objectIntegralregning.a, lower, objectIntegralregning.b, objectIntegralregning.c),f(objectIntegralregning.a, upper, objectIntegralregning.b, objectIntegralregning.c)], color= "green")
         plt.show()
 plot()
